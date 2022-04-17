e7e6861959e005a15a53b9065d1391e53b1f7a40
Lipon Roy <liponbdbd@gmail.com>
2022-04-17 10:38:56 +0600

    this project start from now

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>Project Start</h1>
+			<h1>My Photography</h1>
+		</div>
+	);
+}
+
+export default App;
